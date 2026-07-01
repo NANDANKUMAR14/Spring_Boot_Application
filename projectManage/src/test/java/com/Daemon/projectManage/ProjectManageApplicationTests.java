eef7d852cfec51e1571b8dfcdcceef0240347727
Nandankumar S <nandan1452007@gmail.com>
2026-07-01 23:13:10 +0530

    initalized springboot project

---
+++ projectManage/src/test/java/com/Daemon/projectManage/ProjectManageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Daemon.projectManage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectManageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
